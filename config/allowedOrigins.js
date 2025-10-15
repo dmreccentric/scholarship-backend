2e05f928fcbba2702acdda8b4c36c069c5b4fc8f
dmreccentric <eccentricecc481@gmail.com>
2025-10-15 12:38:54 +0100

    trying to add mailing functionality

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
   "http://localhost:3001", // Next.js dev server
   "http://localhost:3000", // Next.js dev server
   "http://127.0.0.1:3000",
-  "https://scholarship-admin-chi.vercel.app", // Sometimes used in dev
+  "https://scholarship-admin-chi.vercel.app",
+  "https://scholarship-frontend-kappa.vercel.app",
 ];
 
 module.exports = allowedOrigins;
